c60ab1b4e3a1f506c087b369e318638ca78287d5
chandanadev9390 <159886980+chandanadev9390@users.noreply.github.com>
2024-05-16 07:10:56 +0530

    Update product.js

--- product.js
+++ product.js
@@ -2,7 +2,7 @@ const image = document.getElementById('productImg');
 const btn = document.getElementsByClassName( 'btn' );
 
 btn[0].addEventListener( 'click', function(){
-    image.src = '../assets/noise ash.webp';
+    image.src = './noise ash.webp';
     for(bt of btn){
         bt.classList.remove( 'active' );
         
@@ -10,7 +10,7 @@ btn[0].addEventListener( 'click', function(){
     this.classList.add( 'active' )
 });
 btn[1].addEventListener( 'click', function(){
-    image.src = '../assets/noise white.png.jpg';
+    image.src = './noise white.png.jpg';
     for(bt of btn){
         bt.classList.remove( 'active' );
         
@@ -18,7 +18,7 @@ btn[1].addEventListener( 'click', function(){
     this.classList.add( 'active' )
 })
 btn[2].addEventListener( 'click', function(){
-    image.src = '../assets/noise-colorfit-pulse dblue.png.jpg';
+    image.src = './noise-colorfit-pulse dblue.png.jpg';
     for(bt of btn){
         bt.classList.remove( 'active' );
         
@@ -30,7 +30,7 @@ const image1 = document.getElementById('productImg1');
 const btn1 = document.getElementsByClassName( 'btn1' );
 
 btn1[0].addEventListener( 'click', function(){
-    image1.src = '../assets/boat u black.jpeg';
+    image1.src = './boat u black.jpeg';
     for(bt1 of btn1){
         bt1.classList.remove( 'active' );
         
@@ -38,7 +38,7 @@ btn1[0].addEventListener( 'click', function(){
     this.classList.add( 'active' )
 });
 btn1[1].addEventListener( 'click', function(){
-    image1.src = '../assets/u boat bronz.jpeg';
+    image1.src = './u boat bronz.jpeg';
     for(bt1 of btn1){
         bt1.classList.remove( 'active' );
         
@@ -46,7 +46,7 @@ btn1[1].addEventListener( 'click', function(){
     this.classList.add( 'active' )
 })
 btn1[2].addEventListener( 'click', function(){
-    image1.src = '../assets/u boat brown.jpeg';
+    image1.src = './u boat brown.jpeg';
     for(bt1 of btn1){
         bt1.classList.remove( 'active' );
         
@@ -57,7 +57,7 @@ const image2 = document.getElementById('productImg2');
 const btn2 = document.getElementsByClassName( 'btn2' );
 
 btn2[0].addEventListener( 'click', function(){
-    image2.src = '../assets/GPS-Smartwatch-PNG-Transparent-Image.png';
+    image2.src = './GPS-Smartwatch-PNG-Transparent-Image.png';
     for(bt2 of btn2){
         bt2.classList.remove( 'active' );
         
@@ -65,7 +65,7 @@ btn2[0].addEventListener( 'click', function(){
     this.classList.add( 'active' )
 });
 btn2[1].addEventListener( 'click', function(){
-    image2.src = '../assets/GPS-Smartwatch-PNG-Image-Background.png';
+    image2.src = './GPS-Smartwatch-PNG-Image-Background.png';
     for(bt2 of btn2){
         bt2.classList.remove( 'active' );
         
@@ -73,7 +73,7 @@ btn2[1].addEventListener( 'click', function(){
     this.classList.add( 'active' )
 })
 btn2[2].addEventListener( 'click', function(){
-    image2.src = '../assets/GPS-Smartwatch-PNG-Photo.png';
+    image2.src = './GPS-Smartwatch-PNG-Photo.png';
     for(bt2 of btn2){
         bt2.classList.remove( 'active' );
         
